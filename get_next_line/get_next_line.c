4a72d6b5849daddaa300f70c090c99084207241f
dilopezcalle <dilopez@student.42urduliz.com>
2022-05-08 10:46:27 +0200

    mandatory part done

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,42 +6,52 @@
 /*   By: dilopez- <dilopez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/07 15:10:03 by dilopez-          #+#    #+#             */
-/*   Updated: 2022/05/08 09:44:26 by dilopez-         ###   ########.fr       */
+/*   Updated: 2022/05/08 10:45:50 by dilopez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
+char	*ft_read_buffer_nl(char *str, int fd, size_t *bytes_read)
 {
-	static char	*str;
-	char		*aux;
-	char		buffer[BUFFER_SIZE + 1];
-	char		*line;
-	size_t		bytes_read;
+	char	*aux;
+	char	buffer[BUFFER_SIZE + 1];
 
-	bytes_read = BUFFER_SIZE;
-	buffer[BUFFER_SIZE] = '\0';
 	aux = ft_strjoin(str, "");
-	str = NULL;
-	if (!fd || fd == -1)
-		return (NULL);
-	while (bytes_read > 0 && ft_strnl(aux) == -1)
+	while (*bytes_read > 0 && ft_strnl(aux) == -1)
 	{
-		bytes_read = read(fd, buffer, BUFFER_SIZE);
-		if ((int)bytes_read <= 0)
+		*bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if ((int)*bytes_read <= 0)
 		{
-			if ((int)bytes_read == 0)
+			if ((int)*bytes_read == 0)
 				return (aux);
 			free(aux);
 			return (NULL);
 		}
-		buffer[bytes_read] = '\0';
-		if (!aux && bytes_read > 0)
+		buffer[*bytes_read] = '\0';
+		if (!aux)
 			aux = ft_substr(buffer, 0, BUFFER_SIZE);
-		else if (bytes_read > 0)
+		else
 			aux = ft_strjoin(aux, buffer);
 	}
+	return (aux);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*str;
+	char		*aux;
+	char		*line;
+	size_t		bytes_read;
+
+	bytes_read = BUFFER_SIZE;
+	aux = ft_strjoin(str, "");
+	str = NULL;
+	if (fd == -1)
+		return (NULL);
+	aux = ft_read_buffer_nl(aux, fd, &bytes_read);
+	if (!aux)
+		return (NULL);
 	line = ft_substr(aux, 0, ft_strnl(aux));
 	str = ft_substr(aux, ft_strnl(aux), ft_strlen(aux));
 	if (bytes_read <= 0)
@@ -53,7 +63,7 @@ char	*get_next_line(int fd)
 int	main(void)
 {
 	char	*line;
-	int	fd = open("./multiple_nlx5", O_RDONLY);
+	int	fd = open("test_files/multiple_nlx5", O_RDONLY);
 	int	i;
 
 	i = 0;
